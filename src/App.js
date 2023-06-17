f0779f854f8e42383fc880d605c9dc371ca96c74
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-06-18 01:10:46 +0530

    jhvugohiuiop

--- src/App.js
+++ src/App.js
@@ -2,21 +2,10 @@
 import './App.css';
 
 function App() {
-  const handleClick = () => { 
-    alert('clicked');
-  }
   return (
     <div className="App">
-        <button onClick={handleClick}>handleClick</button>
-      {/* <button onClick={handleClick()}>handleClick()</button> */}
-      <button onClick={() => {
-        handleClick();
-      }}>ananymous callback</button>
-        <button onClick={function loki() {
-        handleClick();
-      }}>named callback</button>
+      <h1>Event Listiner</h1>
     </div>
-    
   );
 }
 
