4973efaa375f6c970cc2a6289160c680bb419d14
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-06-17 11:17:46 +0530

    basic -filter-operation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import { useEffect } from 'react';
+import React, { useState} from 'react';
+import { Users } from './Users';
 import './App.css';
 
 function App() {
-  const handleClick = () => { 
-    alert('clicked');
-  }
-
-  useEffect(() => {
-    
-    const propertyButton = document.getElementById('property-button');
-    propertyButton.addEventListener('click', (k) => {
-    console.log(k);
-      // alert('property button clicked');
-    });
-  }, []);
-
+ 
+const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="App">
-      <button onClick={handleClick}>handleClick</button>
-      <button onClick={() => handleClick()}>anonymous callback</button>
-      <button onClick={function loki() { handleClick(); }}>named callback</button>
-      <p>event listener property</p>
-      <button id="property-button">property</button>
+      <input className="" type="text" placeholder="search...." onChange={(event) => {
+        event.preventDefault();
+        setSearchTerm(event.target.value);
+        console.log(searchTerm  )
+      }}/>
+      <ul className="list">
+        {
+          Users.filter((user)=>{ return user.first_name.toLowerCase().includes(searchTerm)}).map((user) => { 
+            return <li className="list-item" key={ user.id}>{ user.first_name}</li>
+          })
+        }
+       
+      </ul>
     </div>
   );
 }
